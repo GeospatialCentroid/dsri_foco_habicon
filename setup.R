b736badb737a9d5cea19d1750236ff00617a27b9
Caitlin Mothes <ccmothes@gmail.com>
2024-08-07 16:29:29 -0600

    first draft of full ENMeval workflow

--- setup.R
+++ setup.R
@@ -30,7 +30,8 @@ packages <- c(
   "akima",
   "tmap",
   "spocc",
-  "furrr")
+  "furrr",
+  "ENMeval")
 
 # tack tidyverse on the end always to keep tidyverse functions as default for function conflicts
 packages <- c(packages, "tidyverse")
